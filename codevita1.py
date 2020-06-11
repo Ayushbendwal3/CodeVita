15ff965a695728870bb335755c252299dadbc6ac
Ayush Bendwal <31592180+Ayushbendwal3@users.noreply.github.com>
2020-06-11 20:32:47 +0530

    Update codevita1.py

--- codevita1.py
+++ codevita1.py
@@ -2,6 +2,10 @@
 #           [1, 4, 4, 5],
 #           [8, 2, 5, 4],
 #           [1, 8, 5, 9]]
+
+#with python list
+
+
 matrix = []
 n = int(input())
 
@@ -29,22 +33,23 @@ for i in range(2*n):
         if matrix[0][1] > matrix[1][0]:
             k = matrix[1][0]
             score = (score//2) + k
-            matrix.pop(0)
+            matrix.pop(0) # removing Row
         else:
             k = matrix[0][1]
             score = (score//2) + k
-            for i in matrix:
+            
+            for i in matrix: # removing column
                 del i[0]
 
     except:
         try:
             k = matrix[0][1]
             score = (score//2) + k
-            for i in matrix:
+            for i in matrix: # removing column
                 del i[0]
         except:
             k = matrix[1][0]
             score = (score//2) + k
-            matrix.pop(0)
+            matrix.pop(0) # removing Row
 
 print(score)
